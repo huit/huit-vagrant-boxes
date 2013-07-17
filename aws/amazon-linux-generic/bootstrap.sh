b0a2bd0dc04adf56ef204bb9a361c0f805ecfc8f
Daniel Silverman <me@agperson.com>
2013-07-17 08:44:05 -0400

    Fixing yum install (adding -y)

--- aws/amazon-linux-generic/bootstrap.sh
+++ aws/amazon-linux-generic/bootstrap.sh
@@ -4,4 +4,4 @@
 rpm -i http://yum.puppetlabs.com/el/6Server/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 
 # Install dependencies
-yum install git rubygems gcc libxml2-devel libxslt-devel puppet
+yum install -y git rubygems gcc libxml2-devel libxslt-devel puppet
