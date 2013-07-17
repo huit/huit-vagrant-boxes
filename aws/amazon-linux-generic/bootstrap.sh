430ec703d4effbb40fbd603c0f96c238cdc9ab00
Daniel Silverman <me@agperson.com>
2013-07-16 23:14:15 -0400

    Checking in the raw compressed AWS box so scripts can reference it (since its tiny ayway)

--- aws/amazon-linux-generic/bootstrap.sh
+++ aws/amazon-linux-generic/bootstrap.sh
@@ -5,4 +5,3 @@ rpm -i http://yum.puppetlabs.com/el/6Server/products/x86_64/puppetlabs-release-6
 
 # Install dependencies
 yum install git rubygems gcc libxml2-devel libxslt-devel puppet
-gem install --no-rdoc --no-ri r10k
