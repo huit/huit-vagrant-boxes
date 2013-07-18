86fe2a542f47b22a08dd9742349762512452a34e
Daniel Silverman <me@agperson.com>
2013-07-17 22:11:14 -0400

    Moving all provisioning scripts into the Vagrantfile since we can't pull in script files from within the box.  Fixing the PTY problem using cloud-init.

--- aws/amazon-linux-generic/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Setup Puppet Labs repository for latest Puppet
-rpm -i http://yum.puppetlabs.com/el/6Server/products/x86_64/puppetlabs-release-6-7.noarch.rpm
-
-# Install dependencies
-yum install -y git rubygems gcc libxml2-devel libxslt-devel puppet
